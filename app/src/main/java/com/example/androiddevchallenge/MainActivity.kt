59c4599b5e9eceadbdc061cc0d4956922b8a38a7
kaga <kaga21@gmail.com>
2021-02-26 02:02:11 +1300

    add navigation

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,14 +15,29 @@
  */
 package com.example.androiddevchallenge
 
+
 import android.os.Bundle
+
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import androidx.ui.foundation.Clickable
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,8 +54,59 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "petlist") {
+        composable("petlist") { PetList(navController = navController) }
+        composable("petprofile") { PetProfile(navController = navController) }
+    }
+}
+
+@Composable
+fun PetList(navController: NavController) {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyColumn(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(10) {
+                PetCard(Pet(name = "Cat"), navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun PetProfile(navController: NavController) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column() {
+            Button(onClick = { navController.popBackStack() }) {
+                Text(text = "Hello")
+            }
+        }
+    }
+}
+
+@Composable
+fun PetCard(pet: Pet, navController: NavController) {
+
+    Card(modifier = Modifier.fillMaxWidth(1f).clickable {
+        navController.navigate("petprofile")
+    },
+        elevation = 2.dp) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = pet.name
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(1f),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(text = pet.name, Modifier.padding(16.dp))
+            }
+
+        }
     }
 }
 
@@ -59,3 +125,5 @@ fun DarkPreview() {
         MyApp()
     }
 }
+
+data class Pet(val name: String)
